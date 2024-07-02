0e4ada2bcd25d167375296c85bdf1b086f3cc043
Tarek Ahmed <tarek.ahmed@mercator.group>
2024-07-02 12:35:28 +0100

    tdd calculate tax and fizzbuzz

---
+++ Scala101/src/main/scala/CollectionsMaps.scala
@@ -0,0 +1,12 @@
+class CollectionsMaps {
+  
+  val testSeq:Seq[String] = Seq("a","b","c")
+  val letter = testSeq(0)
+  
+  val testMap: Map[Int,String] = Map(10->"dog",11->"cat",12->"leopard")
+
+  def main(args: Array[String]): Unit = {
+    
+  }
+
+}
