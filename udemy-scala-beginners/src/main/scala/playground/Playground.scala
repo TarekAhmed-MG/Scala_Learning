058f0ede44552a57b96b397ad0e9698976724a66
Tarek Ahmed <tarek.ahmed@mercator.group>
2024-07-17 15:42:53 +0100

    functions

--- udemy-scala-beginners/src/main/scala/playground/Playground.scala
+++ udemy-scala-beginners/src/main/scala/playground/Playground.scala
@@ -5,4 +5,11 @@ object Playground {
     println("Im ready to start Scala")
   }
 
+  // EXPRESSIONS VS INSTRUCTIONS:
+    // Instructions are executed whereas expressions are evaluated.(think scala code blocks where the last statement is the return value as opposed to a print statement which is an instruction)
+
+  // side effects are basically like instructions in imperative programming like Java,Python,C etc but in scala they are expressions that return unit
+
+  
+
 }
