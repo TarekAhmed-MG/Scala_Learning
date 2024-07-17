c2a03563914123a0d167063d4ccf3007582c1ac5
Tarek Ahmed <tarek.ahmed@mercator.group>
2024-07-17 16:03:16 +0100

    notes on tail recursion

---
+++ udemy-scala-beginners/src/main/scala/lectures/part1basics/Recursion.scala
@@ -0,0 +1,9 @@
+package lectures.part1basics
+
+object Recursion extends App{
+
+  // stack overflow happens when the recursive depth is too big.
+  // Tail Recursion combats this and you put the recursive call as the last statement this makes it tail recursive.
+
+
+}
