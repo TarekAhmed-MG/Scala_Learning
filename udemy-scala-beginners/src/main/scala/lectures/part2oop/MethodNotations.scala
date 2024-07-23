20132dad7134b3fdd77f853f114015124d4ddc21
Tarek Ahmed <tarek.ahmed@mercator.group>
2024-07-23 11:46:08 +0100

    method notation

--- udemy-scala-beginners/src/main/scala/lectures/part2oop/MethodNotations.scala
+++ udemy-scala-beginners/src/main/scala/lectures/part2oop/MethodNotations.scala
@@ -9,9 +9,14 @@ object MethodNotations extends App {
 
     // in scala you have the freedom to name your method as you wish
     def +(person: Person): String = s"${this.name} is hanging out with ${person.name}"
-    def unary_! : String = s"$name, what the heck?!" // you need to have a space between the ! and : otherwise compiler will think its together
+    def +(str:String) = new Person(s"$name ($str)",favoriteMovie)
+    def unary_! : String = s"$name, what the heck?!" // you need to have a space between the ! and : otherwise compiler will think its together only works with +_  tilda and !
+    def unary_+ : Person = new Person(name, favoriteMovie, age + 1)
     def isAlive: Boolean = true
     def apply(): String = s"Hi, my name is $name and I like $favoriteMovie"
+    def apply(n: Int): String = s"$name watched $favoriteMovie $n times"
+    def learns(thing: String) = s"$name is learning $thing"
+    def learnsScala = this learns "Scala"
   }
 
   val mary = new Person("Mary", "Inception")
@@ -49,6 +54,31 @@ object MethodNotations extends App {
   // apply
   println(mary.apply()) // special scala method that allows you to call an object like a method as seen below
   println(mary()) // equivalent
+
+  /*
+      1.  Overload the + operator
+          mary + "the rockstar" => new person "Mary (the rockstar)"
+
+      2.  Add an age to the Person class
+          Add a unary + operator => new person with the age + 1
+          +mary => mary with the age incrementer
+
+      3.  Add a "learns" method in the Person class => "Mary learns Scala"
+          Add a learnsScala method, calls learns method with "Scala".
+          Use it in postfix notation.
+
+      4.  Overload the apply method
+          mary.apply(2) => "Mary watched Inception 2 times"
+     */
+
+  // equivalent
+  println((mary + "the Fighter").apply())
+  println((mary + "the Fighter")())
+
+  println((+mary).age)
+  println(mary learnsScala)
+  println(mary(6))
+
 
 
 
