b1e72b222b17e839d8674d556395f01990b57f5e
Tarek Ahmed <tarek.ahmed@mercator.group>
2024-07-24 17:05:55 +0100

    bound types and solving variance problem

--- udemy-scala-beginners/src/main/scala/lectures/part2oop/Generics.scala
+++ udemy-scala-beginners/src/main/scala/lectures/part2oop/Generics.scala
@@ -44,16 +44,61 @@ object Generics extends App {
   val animal:Animal = new Cat()
   val animalList: CovariantList[Animal] = new CovariantList[Cat]
 
-  // once i declare an animal list to be a covariant list of animal can i add any animal to it such as animalList.add(new Dog) ?
-  // 2) No because a cat and a dog are two different animals => this is called invariance
+  // once i declare an animal list of for example cat to be a covariant list of animal can i add any animal to it such as animalList.add(new Dog) ?
+  // if you do that then you will not have a list of cat anymore but a new list of Animals and it will be more generic explanation below at MyNewList
+
+  // 2) No, this is called invariance
   class InvariantList[A] // you dont add a + in front so this is the normal one you usually seen before
   val invariantAnimalList: InvariantList[Animal] = new InvariantList[Animal] // if you try to put a cat or a dog like above it will not work
 
   // 3) Contravariance - This method is counterintuitive and is the opposite relationship generally a no go
 
   class ContravariantList[-A] // this is denoted with a - operator in front of the type parameter A
+
+  // this gives you a total opposite type relation replacing a list of cats with a list of animals
   val contravariantAnimalList: ContravariantList[Cat] = new ContravariantList[Animal]
 
+  /*
+    This method doesnt make much sense because a Cat is a type of animal so in the case of Covariance you can replace a list of
+    animals with a list of cats as when you look at it in the point of Type Hierarchy a Cat is derived from an Animal,
+    but with Contravariance your replacing a list of cats with a list of animals so you are going from a derived type to a parent type
+    but not all animals are cats. so Covariance doesnt work that well with lists but there is a use case.
+
+   */
+
+  // say we have an animal trainer well in this case we can use a trainer of Animal who can train cats,dogs,birds to also be a trainer of Cat
+  class Trainer[-A]
+  val trainer: Trainer[Cat] = new Trainer[Animal]
+
+  // bounded types
+
+  //Bounded types allow you to use your generic classes only for certain types that are either a subclass of a different type
+  // or a super class of a different type
+
+  class Cage[A <: Animal](animal: A) // here you are saying class parameter A only accepts subtypes of Animal
+  val cage = new Cage(new Dog) // this is acceptable because Dog is a type animal
+
+  class Car
+  //val newCage = new Cage(new Car) => this will not work because a Car is not type Animal
+
+  // <: is an upper bounded type -> only accepts subtypes of a class
+  // >: lower bounded type  -> only accepts supertype of a class
+
+  // bounded types solve the variance problem:
+
+  class MyNewList[+A] {
+    // def add(element:A): MyList[A] = ??? this wouldn't work even though the list is generic and covariant
+    // and its because if you try to add a different subtype to this list for example a Dog to a list of Cats then you will get back a new list of Animal
+    // so you have to define it like this:
+    def add[B>: A](element: B): MyList[B] = ???
+    // this says if in a list of A you put in a element of B which is a supertype of A then this list will turn into a List of B and not A
+    /*
+    example:
+    A = Cat
+    B = Animal
+    if you now pass in a dog to a list of cat then it will turn into a list of animal
+     */
+  }
 
 
 
