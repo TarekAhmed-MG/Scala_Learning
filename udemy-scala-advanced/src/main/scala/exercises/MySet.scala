3e6d2e01d64b20980c5608377da89411d72d7c98
TarekAhmed-MG <173894783+TarekAhmed-MG@users.noreply.github.com>
2024-10-02 17:07:42 +0100

    Create Empty Set Functionality

--- udemy-scala-advanced/src/main/scala/exercises/MySet.scala
+++ udemy-scala-advanced/src/main/scala/exercises/MySet.scala
@@ -3,6 +3,8 @@ package exercises
 trait MySet[A] extends (A => Boolean) {
   //todo: Implement a functional set
 
+  def apply(elem: A) : Boolean = contains(elem)// as the trait returns boolean we need to modify the apply
+  // so the general concept of this trait MySet is to return a boolean if myset contains an element
   def contains(elem: A) : Boolean
   def +(elem: A) : MySet[A]
   def ++(anotherSet: MySet[A]) : MySet[A]
@@ -11,4 +13,26 @@ trait MySet[A] extends (A => Boolean) {
   def flatMap[B](f: A => B): MySet[B]
   def filter(predicate: A => Boolean) : MySet[A]
   def foreach(f: A => Unit) : Unit
+}
+
+class EmptySet[A] extends MySet[A]{
+
+  def contains(elem: A): Boolean = false
+
+  def +(elem: A): MySet[A] = new NonEmptySet[A](elem,this) // creates a new set with the element as the head and this runtime as tail
+
+  def ++(anotherSet: MySet[A]): MySet[A] = anotherSet
+
+  def map[B](f: A => B): MySet[B] = new EmptySet[B]
+
+  def flatMap[B](f: A => B): MySet[B] = new EmptySet[B]
+
+  def filter(predicate: A => Boolean): MySet[A] = this
+
+  def foreach(f: A => Unit): Unit = () // this is the unit () means unit value
+
+}
+
+class NonEmptySet[A](head: A, tail: MySet[A]) extends MySet[A]{
+
 }
