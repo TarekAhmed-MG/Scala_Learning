b9b5a70517c772c7b151ae0b10d9080956dc0f36
TarekAhmed-MG <173894783+TarekAhmed-MG@users.noreply.github.com>
2024-10-03 16:06:20 +0100

    Create MySet Functional Set now working

--- udemy-scala-advanced/src/main/scala/exercises/MySet.scala
+++ udemy-scala-advanced/src/main/scala/exercises/MySet.scala
@@ -1,5 +1,7 @@
 package exercises
 
+import scala.annotation.tailrec
+
 trait MySet[A] extends (A => Boolean) {
   //todo: Implement a functional set
 
@@ -10,7 +12,7 @@ trait MySet[A] extends (A => Boolean) {
   def ++(anotherSet: MySet[A]) : MySet[A]
 
   def map[B](f: A => B): MySet[B]
-  def flatMap[B](f: A => B): MySet[B]
+  def flatMap[B](f: A => MySet[B]): MySet[B]
   def filter(predicate: A => Boolean) : MySet[A]
   def foreach(f: A => Unit) : Unit
 }
@@ -25,7 +27,7 @@ class EmptySet[A] extends MySet[A]{
 
   def map[B](f: A => B): MySet[B] = new EmptySet[B]
 
-  def flatMap[B](f: A => B): MySet[B] = new EmptySet[B]
+  def flatMap[B](f: A => MySet[B]): MySet[B] = new EmptySet[B]
 
   def filter(predicate: A => Boolean): MySet[A] = this
 
@@ -37,18 +39,60 @@ class NonEmptySet[A](head: A, tail: MySet[A]) extends MySet[A]{
 
   def contains(elem: A): Boolean = elem == head || tail.contains(elem)
 
-  def +(elem: A): MySet[A] = 
+  def +(elem: A): MySet[A] =
     if (this contains elem) this // return same instance if its there as set has no duplicates
     else new NonEmptySet[A](elem,this) // otherwise create a new set with the element as head
 
-  def ++(anotherSet: MySet[A]): MySet[A] = tail ++ anotherSet + head // recursively calling ++ method on tail and then adding head
+  /*
+  when you want to concatenate 123 with 45
+      [1 2 3] ++ [4 5] =
+  the function gets the tail and concatenates it with 45
+      [2 3] ++ [4 5] + 1 =
+  this is a recursive function so now the tail (3) is called
+      [3] ++ [4 5] + 1 + 2 =
+  recursive call again will make the tail empty and ++ another set ++ head
+      [] ++ [4 5] + 1 + 2 + 3
+  now the concatenation for the empty set is from above which is another set and then added to the previous heads
+      [4 5] + 1 + 2 + 3 = [4 5 1 2 3]
+     */
+  def ++(anotherSet: MySet[A]): MySet[A] =
+    tail ++ anotherSet + head // recursively calling ++ method on tail and then adding head
 
-  def map[B](f: A => B): MySet[B]
+  def map[B](f: A => B): MySet[B] = (tail map f) + f(head) // tail mapped to f and then f is applied to head and a new MySet[B] is created
 
-  def flatMap[B](f: A => B): MySet[B]
+  def flatMap[B](f: A => MySet[B]): MySet[B] = (tail flatMap f) ++ f(head)
 
-  def filter(predicate: A => Boolean): MySet[A]
+  def filter(predicate: A => Boolean): MySet[A] = {
+    val filteredTail = tail filter predicate
+    if (predicate(head)) filteredTail + head // if predicate head returns true then return filteredtail + head
+    else filteredTail // otherwise dont pass the head
+  }
 
-  def foreach(f: A => Unit): Unit
+  def foreach(f: A => Unit): Unit ={
+    f(head)
+    tail foreach f
+  }
+}
 
+object MySet {
+  /*
+    val s = MySet(1,2,3) = buildSet(seq(1,2,3), [])
+    = buildSet(seq(2,3), [] + 1)
+    = buildSet(seq(3), [1] + 2)
+    = buildSet(seq(), [1, 2] + 3)
+    = [1,2,3]
+   */
+  def apply[A](values: A*): MySet[A] = { // A* is a argVar that allows you to add multiple values of A
+    @tailrec
+    def buildSet(valSeq: Seq[A], acc: MySet[A]): MySet[A] =
+      if (valSeq.isEmpty) acc
+      else buildSet(valSeq.tail, acc + valSeq.head)
+
+    buildSet(values, new EmptySet[A])
+  }
+}
+
+object MySetPlayground extends App {
+  val s = MySet(1, 2, 3, 4)
+  s + 5 ++ MySet(-1, -2) + 3 flatMap (x => MySet(x, 10 * x)) filter (_ % 2 == 0) foreach println
 }
