458b2612a3e900924594bdd9d84b68572ca62c29
TarekAhmed-MG <173894783+TarekAhmed-MG@users.noreply.github.com>
2024-10-04 17:43:11 +0100

    Completed Functional Set

--- udemy-scala-advanced/src/main/scala/exercises/MySet.scala
+++ udemy-scala-advanced/src/main/scala/exercises/MySet.scala
@@ -105,15 +105,15 @@ class PropertyBasedSet[A](property: A => Boolean) extends MySet[A] {
 
   def +(elem: A): MySet[A] = new PropertyBasedSet[A](x => property(x) ||x== elem)
 
-  def ++(anotherSet: MySet[A]): MySet[A]
+  def ++(anotherSet: MySet[A]): MySet[A] = new PropertyBasedSet[A](x => property(x) || anotherSet(x))
 
-  def map[B](f: A => B): MySet[B]
+  def map[B](f: A => B): MySet[B] = politelyFail
 
-  def flatMap[B](f: A => MySet[B]): MySet[B]
+  def flatMap[B](f: A => MySet[B]): MySet[B] = politelyFail
 
   def filter(predicate: A => Boolean): MySet[A] = new PropertyBasedSet[A](x => property(x) && predicate(x))
 
-  def foreach(f: A => Unit): Unit
+  def foreach(f: A => Unit): Unit = politelyFail
 
   def -(elem: A): MySet[A] = filter(x => x != elem)// removing an element
 
